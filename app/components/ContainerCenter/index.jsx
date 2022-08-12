37297c46d9ac550e1d32f459a048ef8f2ce50b15
Joshua Graber <joshua.d.graber@gmail.com>
2022-08-12 15:58:58 -0400

    Add ContainerCenter component

---
+++ app/components/ContainerCenter/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import style from './style.css';
+
+// EXT LIBS
+import clsx from 'clsx';
+
+export function links() {
+	return [{ rel: 'stylesheet', href: style }];
+}
+
+export const ContainerCenter = ({ children, disabled }) => {
+	const classes = clsx('jdg-container-center', {
+		'jdg-container-center-disabled': disabled,
+	});
+
+	return <div className={classes}>{children}</div>;
+};
