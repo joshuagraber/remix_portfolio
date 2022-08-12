c750775b9be86f0a673abec903160c62ecb5b81b
Joshua Graber <joshua.d.graber@gmail.com>
2022-08-12 16:46:08 -0400

    Add header component

---
+++ app/components/Header/index.jsx
@@ -0,0 +1,28 @@
+// GLOBALS
+import style from './style.css';
+
+// COMPONENTS
+import { ContainerCenter, links as containerCenterLinks } from '../ContainerCenter';
+
+// EXT LIBS
+import clsx from 'clsx';
+
+export function links() {
+	return [...containerCenterLinks(), { rel: 'stylesheet', href: style }];
+}
+
+export const Header = () => {
+	const classes = clsx('jdg-header');
+
+	return (
+		<div className={classes}>
+			<ContainerCenter>
+				<div>Header Placeholder</div>
+				{/* Name / headline to the left */}
+				{/* Nav to the right (hidden, opens from right side of the screen 
+				on desk, from top on mobile, use aria-expanded) */}
+				{/* DarkModeToggle furthest right, always visible */}
+			</ContainerCenter>
+		</div>
+	);
+};
