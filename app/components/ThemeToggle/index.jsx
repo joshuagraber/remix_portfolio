484212829413b65fe2ba8b625f8e97a9ca09c88c
Joshua Graber <joshua.d.graber@gmail.com>
2022-08-16 09:04:02 -0400

    Build ThemeToggle: First draft of styles

--- app/components/ThemeToggle/index.jsx
+++ app/components/ThemeToggle/index.jsx
@@ -1,21 +1,30 @@
 // UTIL
 import { useTheme } from '~/theme';
 
+import styles from './styles.css';
+
+export function links() {
+	return [{ rel: 'stylesheet', href: styles }];
+}
+
 export const ThemeToggle = () => {
 	const { theme, toggleTheme } = useTheme();
 	const isDarkMode = theme === 'jdg-dark-mode';
 
+	const handleChange = () => {
+		toggleTheme();
+	};
+
 	return (
-		<input
-			checked={isDarkMode}
-			className='jdg-theme-toggle'
-			id='jdg-theme-toggle'
-			onChange={toggleTheme}
-			type='checkbox'
-		>
-			<label for='jdg-theme-toggle' class='jdg-theme-toggle-label'>
-				<span class='jdg-theme-toggle-label-background' />
-			</label>
-		</input>
+		<div className='jdg-theme-toggle'>
+			<input
+				checked={isDarkMode}
+				className='jdg-theme-toggle-input'
+				id='jdg-theme-toggle'
+				onChange={handleChange}
+				type='checkbox'
+			/>
+			<label htmlFor='jdg-theme-toggle' className='jdg-theme-toggle-label'></label>
+		</div>
 	);
 };
