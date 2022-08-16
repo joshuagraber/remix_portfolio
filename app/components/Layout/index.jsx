2d0ee53654b054b88b2e252a87de076047cd4d7f
Joshua Graber <joshua.d.graber@gmail.com>
2022-08-16 09:05:07 -0400

    Update / and Layout styles

--- app/components/Layout/index.jsx
+++ app/components/Layout/index.jsx
@@ -13,7 +13,7 @@ export const Layout = ({ children }) => {
 	return (
 		<>
 			<Header />
-			{children}
+			<main>{children}</main>
 			<Footer />
 		</>
 	);
--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,27 +1,10 @@
+// COMPONENTS
+import { Layout, links as layoutLinks } from '~/components/Layout';
+
+export function links() {
+	return [...layoutLinks()];
+}
+
 export default function Index() {
-	return (
-		<div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-			<h1>Welcome to Remix</h1>
-
-			<ul>
-				<li>
-					<a target='_blank' href='https://remix.run/tutorials/blog' rel='noreferrer'>
-						15m Quickstart Blog Tutorial
-					</a>
-				</li>
-
-				<li>
-					<a target='_blank' href='https://remix.run/tutorials/jokes' rel='noreferrer'>
-						Deep Dive Jokes App Tutorial
-					</a>
-				</li>
-
-				<li>
-					<a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
-						Remix Docs
-					</a>
-				</li>
-			</ul>
-		</div>
-	);
+	return <Layout>Home Page</Layout>;
 }
