9bfa02cdd674bfd27eaaeae64373d41cfbe48e9f
Joshua Graber <joshua.d.graber@gmail.com>
2022-08-20 20:10:16 -0400

    Build footer and SVG/Social component

---
+++ app/components/Footer/data.js
@@ -0,0 +1,6 @@
+export const SOCIAL_LINKS = [
+	['email', 'mailto:jdg@joshuadgraber.com'], // TODO: Use a modal, send to backend script
+	['twitter', 'https://www.twitter.com/joshuadgraber'],
+	['github', 'https://www.github.com/joshuagraber'],
+	['linkedin', 'https://www.linkedin.com/in/joshuadgraber'],
+];
--- app/components/Footer/index.jsx
+++ app/components/Footer/index.jsx
@@ -1,23 +1,47 @@
 // GLOBALS
+import { Link } from '@remix-run/react';
 import styles from './styles.css';
-
-// COMPONENTS
-import { ContainerCenter, links as containerCenterLinks } from '../ContainerCenter';
 
 // EXT LIBS
 import clsx from 'clsx';
 
+// HOOKS
+import { useTheme } from '~/theme';
+
+// COMPONENTS
+import { ContainerCenter, links as containerCenterLinks } from '../ContainerCenter';
+import { SocialIcon } from '../SVG/Social';
+
+// CONSTANTS
+import { SOCIAL_LINKS } from './data';
+
+// EXPORTS
 export function links() {
 	return [...containerCenterLinks(), { rel: 'stylesheet', href: styles }];
 }
 
 export const Footer = () => {
 	const classes = clsx('jdg-footer');
+	const { theme } = useTheme();
+	const iconColor = theme === 'jdg-light-mode' ? 'black' : 'white';
 
 	return (
 		<div className={classes}>
 			<ContainerCenter>
-				<div>Footer Placeholder</div>
+				<div className='jdg-footer-attribution'>
+					&copy; Joshua D. Graber, {new Date().getFullYear()}
+				</div>
+				<div className='jdg-footer-contact'>
+					{SOCIAL_LINKS.map((link) => {
+						const [type, url] = link;
+						return (
+							<a href={url} key={type} rel='noreferrer' target='blank'>
+								{' '}
+								<SocialIcon color={iconColor} type={type} />{' '}
+							</a>
+						);
+					})}
+				</div>
 			</ContainerCenter>
 		</div>
 	);
