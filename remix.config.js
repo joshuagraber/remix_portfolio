5749069fe097fcc49041a1dffef1aef5dee0138d
Joshua Graber <joshua.d.graber@gmail.com>
2022-08-24 16:15:30 -0400

    Update config

--- remix.config.js
+++ remix.config.js
@@ -1,7 +1,8 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
 	serverDependenciesToBundle: ['ssr-window'],
-	ignoredRouteFiles: ['**/.*', '.*', '**/*.css', '**/*.css.map', '**/*.scss'],
+	ignoredRouteFiles: ['**/.*', '.*', '*.css', '*.css.map', '*.scss'],
+	// server: './server.js',
 	// appDirectory: "app",
 	// assetsBuildDirectory: "public/build",
 	// serverBuildPath: "build/index.js",
