edd6dcf99b1da0b1227b01e0f4c304c46a93e1d2
Joshua Graber <joshua.d.graber@gmail.com>
2022-08-30 12:42:32 -0400

    Build: show full home page animation only once

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -9,6 +9,7 @@ import { ContainerCenter, links as containerCenterLinks } from '~/components/Con
 import { Layout, links as layoutLinks } from '~/components/Layout';
 
 // HOOKS
+import { useIsFirstTimeVisitor } from '~/hooks/useIsFirstTimeVisitor';
 import { useTheme } from '~/theme';
 
 // ASSETS
@@ -27,47 +28,65 @@ export default function Index() {
 
 	// HOOKS - CUSTOM
 	const { theme } = useTheme();
+	const { isFirstTimeVisitor } = useIsFirstTimeVisitor();
 
 	// HOOKS - EFFECTS
 	React.useEffect(() => {
-		// TODO: Add LS check and hook to run the page load animation only once per user even on subsequent visits
-		const timeout = setTimeout(() => {
+		let timeout = null;
+		if (isFirstTimeVisitor) {
+			timeout = setTimeout(() => {
+				setimageIsHidden(false);
+				setTextIsHidden(false);
+			}, 1000); // Timeout should match CSS animation delay
+		}
+
+		// Checking false explicitly because the hook returns undefined if unset
+		if (isFirstTimeVisitor === false) {
 			setimageIsHidden(false);
 			setTextIsHidden(false);
-		}, 2000);
+		}
 
-		return () => {
-			clearTimeout(timeout);
-		};
-	}, []);
+		if (timeout) {
+			return () => {
+				clearTimeout(timeout);
+			};
+		}
+	}, [isFirstTimeVisitor]);
 
 	// VARS
 	const arrowStroke = theme === 'jdg-light-mode' ? 'black' : 'white';
+	const pending = typeof isFirstTimeVisitor === 'undefined';
 
 	// CLASSES
 	const classes = clsx('jdg-home-container-center', {
 		'jdg-home-container-center-hide-text': textIsHidden,
 		'jdg-home-container-center-hide-image': imageIsHidden,
+		'jdg-home-container-center-no-animation': isFirstTimeVisitor === false,
+		'jdg-home-container-center-show-animation': isFirstTimeVisitor,
 	});
 
 	return (
 		<Layout>
 			<ContainerCenter className={classes}>
-				<div className='jdg-home-image-container'>
-					<figure className='jdg-home-image'>
-						<img src={headshot} alt='Joshua D. Graber close-up headshot' />
-						<figcaption>Photo by Grace Rosselli</figcaption>
-					</figure>
-				</div>
-				<div className='jdg-home-text-container'>
-					<h2>Writing</h2>
-					<h2>Editing</h2>
-					<h2>JavaScript development</h2>
+				{!pending && (
+					<>
+						<div className='jdg-home-image-container'>
+							<figure className='jdg-home-image'>
+								<img src={headshot} alt='Joshua D. Graber close-up headshot' />
+								<figcaption>Photo by Grace Rosselli</figcaption>
+							</figure>
+						</div>
+						<div className='jdg-home-text-container'>
+							<h2>Writing</h2>
+							<h2>Editing</h2>
+							<h2>JavaScript development</h2>
 
-					<Link prefetch='intent' to='/about'>
-						Learn more <Arrow stroke={arrowStroke} direction='right' />
-					</Link>
-				</div>
+							<Link prefetch='intent' to='/about'>
+								Learn more <Arrow stroke={arrowStroke} direction='right' />
+							</Link>
+						</div>
+					</>
+				)}
 			</ContainerCenter>
 		</Layout>
 	);
