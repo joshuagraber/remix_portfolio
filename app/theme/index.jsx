c679128fb21a3bd025fcb14c35c17f816615a694
Joshua Graber <joshua.d.graber@gmail.com>
2022-09-20 15:35:10 -0400

    Update theming to check user preferences first

--- app/theme/index.jsx
+++ app/theme/index.jsx
@@ -1,26 +1,47 @@
 import React from 'react';
 
-// Util
+// UTIL
 import { useLayoutEffect } from '~/hooks/useLayoutEffectSSR';
 
+// CONSTANTS
+import { DEFAULT_THEME } from '~/utils/constants';
+
+// CONTEXT
 const ThemeContext = React.createContext();
 
 export const ThemeProvider = ({ children }) => {
 	// HOOKS
-	const [theme, setTheme] = React.useState('jdg-dark-mode');
+	const [theme, setTheme] = React.useState('unset');
 
 	// VARS
+	let themeInUserPreferences = null;
 	let themeInLocalStorage = null;
 
-	// First check LS
+	// Check LS
 	if (typeof localStorage !== 'undefined') {
 		themeInLocalStorage = localStorage.getItem('theme');
 	}
 
+	if (typeof window !== 'undefined') {
+		const userPrefersDarkTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+		const userPrefersLightTheme = window.matchMedia('(prefers-color-scheme: light)').matches;
+
+		if (userPrefersDarkTheme) {
+			themeInUserPreferences = 'jdg-dark-mode';
+		}
+		if (userPrefersLightTheme) {
+			themeInUserPreferences = 'jdg-light-mode';
+		}
+	}
+
 	// If exists, set theme to one found in LS on first render only
 	React.useEffect(() => {
-		if (themeInLocalStorage) {
-			setTheme(themeInLocalStorage);
+		// LS first preference, if user has been here before,
+		// otherwise go with user preferences,
+		// otherwise default
+		const themeToSet = themeInLocalStorage ?? themeInUserPreferences ?? DEFAULT_THEME;
+		if (themeToSet) {
+			setTheme(themeToSet);
 		}
 	}, []);
 
--- app/utils/constants.js
+++ app/utils/constants.js
@@ -1,7 +1,10 @@
+export const DEFAULT_THEME = 'jdg-dark-mode';
+
+export const NAV_ROUTES = ['work', 'experiments', 'blog'];
+
 export const SOCIAL_LINKS = [
 	['email', ''],
 	['twitter', 'https://www.twitter.com/joshuadgraber'],
 	['github', 'https://www.github.com/joshuagraber'],
 	['linkedin', 'https://www.linkedin.com/in/joshuadgraber'],
 ];
-export const NAV_ROUTES = ['work', 'experiments', 'blog'];
