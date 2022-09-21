86e53f5b8ae1d2e3e342e33f6e25f0608c7eac12
Joshua Graber <joshua.d.graber@gmail.com>
2022-09-21 19:21:38 -0400

    Update css processing scripts and postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,8 @@ import autoprefixer from 'autoprefixer';
 
 module.exports = {
 	plugins: {
-		autoprefixer: autoprefixer({ browsers: ['last 4 versions', 'iOS >= 8', 'not dead', '<.2%'] }),
+		autoprefixer: autoprefixer({
+			browsers: ['last 4 versions', 'Firefox ESR', 'iOS >= 8', 'not dead', '<.2%'],
+		}),
 	},
 };
