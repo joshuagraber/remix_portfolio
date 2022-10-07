cb591af0d4f876ceae93fdda1651409d03ba6d4b
Joshua Graber <joshua.d.graber@gmail.com>
2022-10-07 11:47:14 -0400

    Update: ContactFormResponseMessage: - Add transition styles

--- app/components/ContactForm/ResponseMessage/index.jsx
+++ app/components/ContactForm/ResponseMessage/index.jsx
@@ -1,31 +1,62 @@
 // GLOBALS
-import { bool, oneOf, shape, string, undefined } from 'prop-types';
+import React from 'react';
+import { bool, oneOf, oneOfType, shape, string } from 'prop-types';
+import { CSSTransition } from 'react-transition-group';
 
-export const ContactFormResponseMessage = ({ data, isResponseFinished }) => {
+// EXTERNAL LIBS
+import clsx from 'clsx';
+
+export const ContactFormResponseMessage = ({ data, isDisplayed, isResponseFinished }) => {
+	// HOOKS - REF
+	const ref = React.useRef(null);
+
 	// VARS
 	const isError = data?.error;
 	const isErrorMessageFinishedDisplaying = isError && isResponseFinished;
 
 	const renderErrorMessage = data && isError && !isErrorMessageFinishedDisplaying;
 	const renderSuccessMessage = data && !isError;
+	const renderMessage = (renderErrorMessage || renderSuccessMessage) && isDisplayed;
 	const responseMessage = data?.success ?? data?.error;
 
-	if (renderErrorMessage || renderSuccessMessage) {
-		return (
-			<div className='jdg-contact-form-response'>
-				<h5>{responseMessage.heading}</h5>
-				<p>{responseMessage.body}</p>
-			</div>
-		);
-	}
-	return null;
+	const transitionProps = {
+		classNames: 'jdg-contact-form-response',
+		in: isDisplayed,
+		mountOnEnter: true,
+		nodeRef: ref,
+		timeout: 300,
+		unmountOnExit: true,
+	};
+
+	return (
+		renderMessage && (
+			<CSSTransition {...transitionProps}>
+				{(state) => {
+					const classes = clsx('jdg-contact-form-response', {
+						// Transitions
+						'jdg-contact-form-response-entering': state === 'entering',
+						'jdg-contact-form-response-entered': state === 'entered',
+						'jdg-contact-form-response-exiting': state === 'exiting',
+						'jdg-contact-form-response-exited': state === 'exited',
+					});
+
+					return (
+						<div className={classes}>
+							<h5>{responseMessage.heading}</h5>
+							<p>{responseMessage.body}</p>
+						</div>
+					);
+				}}
+			</CSSTransition>
+		)
+	);
 };
 
 ContactFormResponseMessage.propTypes = {
-	data: oneOf([
+	data: oneOfType([
 		shape({ current: shape({ headline: string, body: string }) }),
 		shape({ error: shape({ headline: string, body: string }) }),
-		undefined,
 	]),
-	isResponseFinished: bool.isRequired,
+	isDisplayed: bool,
+	isResponseFinished: bool,
 };
